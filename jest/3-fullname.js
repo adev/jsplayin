0e06028183b05a942924d569a0ac20f4dae98f51
Andy de Vale <andy.devale@workvisible.com>
2018-03-03 17:11:26 +0000

    Completed jest matcher challenge 3

---
+++ jest/3-fullname.js
@@ -0,0 +1,6 @@
+module.exports = fullname;
+
+function fullname(name){
+    full = name.split(' ');
+    return {first: full[0], last: full[1]};
+}
---
+++ jest/3-fullname.test.js
@@ -0,0 +1,8 @@
+const fullname = require('./3-fullname');
+
+test('Turns John Smith into a javascript object', () => {
+    let thename = fullname('John Smith');
+    expect(thename).toEqual({first: 'John', last:'Smith'});
+    expect(thename.first).toBe('John');
+    expect(thename.last).toBe('Smith');
+});
