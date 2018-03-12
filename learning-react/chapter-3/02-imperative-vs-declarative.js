97a48f609e7842a5991433bee5fa54300c029cee
Andy de Vale <andy.devale@workvisible.com>
2018-03-12 13:49:17 +0000

    Learning React, chapter 3, new file no new code

---
+++ learning-react/chapter-3/02-imperative-vs-declarative.js
@@ -0,0 +1,8 @@
+/*
+Functional programming is part of a larger programming paradigm: declarative
+programming.
+
+Declarative programming is a style of programming where applications are structured
+in a way that prioritises describing what should happen over defining how it
+should happen.
+*/
