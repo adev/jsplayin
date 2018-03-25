8c61f891fadaaa35acfa3b651e41ebc3c605b829
Andy de Vale <andy.devale@workvisible.com>
2018-03-25 18:17:21 +0100

    Coderbyte #9 time-convert

---
+++ coderbyte/09-time-convert.js
@@ -0,0 +1,4 @@
+const timeConvert = num =>
+  Math.floor(num / 60) + ':' + num % 60
+
+module.exports = timeConvert
--- coderbyte/09-time-convert.test.js
+++ coderbyte/09-time-convert.test.js
@@ -10,3 +10,11 @@
 //
 // Input:45
 // Output:"0:45"
+
+const timeConvert = require('./09-time-convert')
+
+test('Coderbyte, time convert', () => {
+  expect(timeConvert(60)).toBe('1:0')
+  expect(timeConvert(126)).toBe('2:6')
+  expect(timeConvert(45)).toBe('0:45')
+})
